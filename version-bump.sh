54aec6ae4ec524b78b417939e640d5f91c897681
amarjeetbatra <amarjeet.batra@avenuebank.com.au>
2024-08-22 13:47:40 +1000

    EZBG-1082:feat:patch skip ci

--- version-bump.sh
+++ version-bump.sh
@@ -71,7 +71,7 @@ else
   mvn --file $POMPATH/pom.xml -q versions:set -DnewVersion="${NEW_VERSION}"
   git add $POMPATH/pom.xml
   REPO="https://$GITHUB_ACTOR:$TOKEN@github.com/$GITHUB_REPOSITORY.git"
-  git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION"
+  git commit -m "Bump pom.xml from $OLD_VERSION to $NEW_VERSION [skip ci]"
   git tag $NEW_VERSION
   git push $REPO --follow-tags
   git push $REPO --tags
